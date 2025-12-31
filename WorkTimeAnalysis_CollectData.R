9874c5f3c0ed02c9e1fd03826c98d5b4e4d542c7
Sam Zipper <samzipper@ku.edu>
2025-12-30 20:25:00 -0800

    Get 2025 data

--- WorkTimeAnalysis_CollectData.R
+++ WorkTimeAnalysis_CollectData.R
@@ -7,6 +7,53 @@ library(googlesheets4)
 # deauthorize - no need to write or read private sheets
 gs4_deauth()
 
+
+## collect for 2025
+sheet_url <- "https://docs.google.com/spreadsheets/d/11kV0dOk0yffU0RUE0mnBRTkFdp7VOKd4RSpKylAc2cQ/edit"
+sheet_info <- gs4_get(sheet_url)
+tabs_all <- 
+  sheet_info$sheets %>% 
+  subset(name != "template")
+
+for (w in 1:length(tabs_all$name)){
+  # figure out when this week starts
+  week_start_date <- lubridate::mdy(tabs_all$name[w])
+  
+  # read google sheet
+  data_all <- read_sheet(sheet_url, sheet = tabs_all$name[w], range = "A2:H40", col_types = "cccccccc", col_names = F)
+  daytype <- data_all[1, 2:8]
+  colnames(daytype) <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  
+  data <- data_all[-c(1:2), ]
+  colnames(data) <- c("Time", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  
+  # transform to long-form and combine
+  daytype_long <- 
+    tibble::tibble(Day = colnames(daytype), 
+                   Daytype = c(as.character(daytype[1,])))
+  data_long <- 
+    reshape2::melt(data, id = "Time", value.name = "Activity", variable.name = "Day")
+  
+  week_summary <- 
+    dplyr::left_join(data_long, daytype_long, by = "Day") %>% 
+    replace_na(list("Activity" = "Not Work")) %>% 
+    dplyr::mutate(week_start_date = week_start_date)
+  
+  if (w == 1){
+    all_weeks <- week_summary
+  } else {
+    all_weeks <-
+      dplyr::bind_rows(week_summary, all_weeks)
+  }
+  
+  # pause to avoid rate limit
+  Sys.sleep(10)
+  
+}
+
+# save output
+write_csv(all_weeks, "WorkTimeAnalysis_Hours_2025.csv")
+
 ## collect for 2024
 sheet_url <- "https://docs.google.com/spreadsheets/d/1Fg9g4pOHfuKfXeZ-mYbUPiZfkmrv0_dsZwzg8JbFpCA/edit"
 sheet_info <- gs4_get(sheet_url)
